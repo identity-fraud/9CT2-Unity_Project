9c849ec6c41044e0c9d95de5a1270b62c5f2d109
transaction-fraud <william.kim22@education.nsw.gov.au>
2025-10-18 09:12:58 +1100

    Dash script after images

--- Assets/Scripts/Dash.cs
+++ Assets/Scripts/Dash.cs
@@ -2,32 +2,75 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    public GameObject PlayerPrefab;
     private Vector3 facing = Vector3.right;
-    private PlayerController Controller;
-    private float time;
+    private PlayerController controller;
+    private bool isDashing = false;
+    private float dashTimer;
+    private float afterImageTimer;
 
-     void Start()
+    private Rigidbody2D rb;
+
+    void Start()
     {
-        Controller = GetComponent<PlayerController>();
+        controller = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        time = Time.deltaTime - 1;
-
-        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E))
+        if (isDashing)
         {
             transform.Translate(facing * 40 * Time.deltaTime);
+            dashTimer -= Time.deltaTime;
+            afterImageTimer -= Time.deltaTime;
+
+            if (afterImageTimer <= 0f)
+
+            {
+                AfterImage();
+                afterImageTimer = 0.01f;
+            }
+
+            if (dashTimer <= 0f)
+            {
+                isDashing = false;
+                rb.gravityScale = 5f;
+            }
         }
 
-        if (Controller.moving.x == 1)
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E))
+        {
+            StartDash();
+        }
+
+        if (controller.moving.x == 1)
         {
             facing = Vector3.right;
         }
-        else if (Controller.moving.x == -1)
+        else if (controller.moving.x == -1)
         {
             facing = Vector3.left;
         }
     }
 
+    void StartDash()
+    {
+        isDashing = true;
+        dashTimer = 0.25f;
+        rb.gravityScale = 0f;
+        afterImageTimer = 0f;
+        rb.linearVelocity = Vector2.zero;
+
+    }
+    void AfterImage()
+    {
+    GameObject clone = Instantiate(PlayerPrefab, transform.position, transform.rotation);
+    SpriteRenderer sr = clone.GetComponent<SpriteRenderer>();
+    if (sr != null)
+    {
+        sr.color = new Color(1f, 1f, 1f, 0.5f); // semi-transparent
+    }
+    Destroy(clone, 0.5f);
+    }
 }
